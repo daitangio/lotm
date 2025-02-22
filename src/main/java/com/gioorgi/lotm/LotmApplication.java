7cf9c76e8b771174ac458911f8baa3ca9bbeff4e
Giovanni Giorgi (macmini) <jj@gioorgi.com>
2025-02-22 19:06:08 +0100

    Lord of the month skeleton by Spring initializer

---
+++ src/main/java/com/gioorgi/lotm/LotmApplication.java
@@ -0,0 +1,13 @@
+package com.gioorgi.lotm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gioorgi/lotm/LotmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gioorgi.lotm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
