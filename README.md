bfea6837fb7312d5035e77f0dd5c5d88ad5d1031
Giovanni Giorgi (macmini) <jj@gioorgi.com>
2025-03-23 18:04:46 +0100

    Basic structure

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # lotm
+
+
+# Resources in order of importance
+https://github.com/mam-dev/cosmo
+https://github.com/caldav4j/caldav4j
+
+
+Google search
+
+https://www.google.com/search?q=spring+based+calendar+server+implementation&client=safari&sca_esv=74940b13bb8c626e&rls=en&sxsrf=AHTn8zq11977mqL0_wpDeQV5ylHJNMEeZg%3A1741032866554&ei=og3GZ9ClIc767_UPju7sqAU&ved=0ahUKEwjQ_rXs3O6LAxVO_bsIHQ43G1UQ4dUDCBA&oq=spring+based+calendar+server+implementation&gs_lp=Egxnd3Mtd2l6LXNlcnAiK3NwcmluZyBiYXNlZCBjYWxlbmRhciBzZXJ2ZXIgaW1wbGVtZW50YXRpb25IAFAAWABwAHgAkAEAmAEAoAEAqgEAuAEMyAEAmAIAoAIAmAMAkgcAoAcA&sclient=gws-wiz-serp
--- src/test/java/com/gioorgi/lotm/LotmApplicationTests.java
+++ src/test/java/com/gioorgi/lotm/LotmApplicationTests.java
@@ -1,20 +1,37 @@
 package com.gioorgi.lotm;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-
+import lombok.extern.slf4j.Slf4j;
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.*;
+import net.fortuna.ical4j.model.component.*;
+import net.fortuna.ical4j.model.parameter.Cn;
+import net.fortuna.ical4j.model.parameter.Role;
+import net.fortuna.ical4j.model.property.Attendee;
+import net.fortuna.ical4j.model.property.ProdId;
+import net.fortuna.ical4j.model.property.Uid;
+import net.fortuna.ical4j.model.property.XProperty;
+import net.fortuna.ical4j.model.property.immutable.ImmutableCalScale;
+import net.fortuna.ical4j.util.RandomUidGenerator;
+import net.fortuna.ical4j.util.UidGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+import java.net.URI;
+import java.time.*;
+import java.util.GregorianCalendar;
 
 import com.gioorgi.lotm.repository.VersionHistoryRepository;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 @SpringBootTest
@@ -40,6 +57,127 @@ class LotmApplicationTests {
 		log.info("System timezone shgould be Rome:{}", ZoneId.systemDefault());
 		assertEquals("Europe/Rome", ""+ZoneId.systemDefault());
     }
+
+	//@Test
+	void parseICalendar() throws IOException, ParserException {
+		FileInputStream fin = new FileInputStream("./MailAttachmentTemplate.ics");
+		CalendarBuilder builder = new CalendarBuilder();
+		Calendar calendar = builder.build(fin);
+		log.info("Calendar: {}",calendar);
+		log.info("{}", calendar.getProperties());
+	}
+
+
+	static int counter=0;
+
+	/**
+	 * Refer to https://www.ical4j.org/examples/model/ for usage examples
+	 * Test to show how to create facncy *.ics from csv source
+	 * TODO: Special 'Show as busy' annotation is not working as expected on Outlook
+	 * @param start_day
+	 * @param start_month
+	 * @param start_hour
+	 * @param start_minute
+	 * @param end_hour
+	 * @param end_minute
+	 * @param description
+	 * @throws IOException
+	 */
+	@ParameterizedTest
+	@CsvFileSource(resources={"/test-cals.csv"}, numLinesToSkip = 1)
+	void fluentApiExample(
+			int start_day, int start_month, int start_hour, int start_minute, int end_hour, int end_minute,
+			String description
+	) throws IOException {
+		counter++; // used for unique filenames
+		/** Template
+		 * BEGIN:VCALENDAR
+		 * METHOD:REQUEST
+		 * PRODID:Microsoft Exchange Server 2010
+		 * VERSION:2.0
+		 * BEGIN:VTIMEZONE
+		 * TZID:W. Europe Standard Time
+		 * BEGIN:STANDARD
+		 * DTSTART:16010101T030000
+		 * TZOFFSETFROM:+0200
+		 * TZOFFSETTO:+0100
+		 * RRULE:FREQ=YEARLY;INTERVAL=1;BYMONTH=10;BYDAY=-1SU
+		 * END:STANDARD
+		 * BEGIN:DAYLIGHT
+		 * DTSTART:16010101T020000
+		 * TZOFFSETFROM:+0100
+		 * TZOFFSETTO:+0200
+		 * RRULE:FREQ=YEARLY;INTERVAL=1;BYMONTH=3;BYDAY=-1SU
+		 * END:DAYLIGHT
+		 * END:VTIMEZONE
+		 * BEGIN:VEVENT
+		 * ORGANIZER;CN=Giovanni Giorgi:mailto:outlook_4B3F58AA39F17B00@outlook.com
+		 * ATTENDEE;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP=TRUE;CN=Giovanni Giorgi:mailto:jj@gioorgi.com
+		 * DESCRIPTION;LANGUAGE=en-US:\n
+		 * UID:040000008200E00074C5B7101A82E00800000000BB903F42D389DB01000000000000000010000000F3EBFC71DD51584C96E39113FA63F4A2
+		 * SUMMARY;LANGUAGE=en-US:Fisiatra
+		 * DTSTART;TZID=W. Europe Standard Time:20250310T140000
+		 * DTEND;TZID=W. Europe Standard Time:20250310T150000
+		 * CLASS:PUBLIC
+		 * PRIORITY:5
+		 * DTSTAMP:20250228T112358Z
+		 * TRANSP:OPAQUE
+		 * STATUS:CONFIRMED
+		 * SEQUENCE:0
+		 * LOCATION;LANGUAGE=en-US:
+		 * X-MICROSOFT-CDO-APPT-SEQUENCE:0
+		 * X-MICROSOFT-CDO-OWNERAPPTID:2123556283
+		 * X-MICROSOFT-CDO-BUSYSTATUS:TENTATIVE
+		 * X-MICROSOFT-CDO-INTENDEDSTATUS:OOF
+		 * X-MICROSOFT-CDO-ALLDAYEVENT:FALSE
+		 * X-MICROSOFT-CDO-IMPORTANCE:1
+		 * X-MICROSOFT-CDO-INSTTYPE:0
+		 * X-MICROSOFT-DONOTFORWARDMEETING:FALSE
+		 * X-MICROSOFT-DISALLOW-COUNTER:FALSE
+		 * X-MICROSOFT-REQUESTEDATTENDANCEMODE:DEFAULT
+		 * X-MICROSOFT-ISRESPONSEREQUESTED:TRUE
+		 * X-MICROSOFT-LOCATIONS:[]
+		 * BEGIN:VALARM
+		 * DESCRIPTION:REMINDER
+		 * TRIGGER;RELATED=START:-PT15M
+		 * ACTION:DISPLAY
+		 * END:VALARM
+		 * END:VEVENT
+		 * END:VCALENDAR
+		 */
+		UidGenerator ug = new RandomUidGenerator();
+		Calendar calendar = new Calendar().withProdId("-//Escape Server LotM//iCal4j 1.0//EN")
+				.withDefaults().getFluentTarget();
+
+		// Add 20 minutes before, to be sure we have time to go there
+
+		LocalDateTime start = LocalDateTime.of(2025, start_month, start_day, start_hour, start_minute);
+		var startCorrected = start.minusMinutes(20);
+
+		var meeting = new VEvent(
+				startCorrected,
+				LocalDateTime.of(2025, start_month, start_day, end_hour, end_minute), description)
+				;
+		var out=calendar
+				.withDefaults()
+				.withComponent(meeting)
+
+				// X-MICROSOFT-CDO-BUSYSTATUS:TENTATIVE
+				// X-MICROSOFT-CDO-INTENDEDSTATUS:OOF
+				.withProperty(new XProperty("X-MICROSOFT-CDO-BUSYSTATUS","TENTATIVE"))
+				.withProperty(new XProperty("X-MICROSOFT-CDO-INTENDEDSTATUS","OOF"))
+				.withProperty(ug.generateUid()).getFluentTarget();
+		log.info("Output:\n{}", out );
+		String fname=counter+"-"+description
+				// Sanitaize bad stuff
+				.replaceAll("[+= ]","-")
+				.replaceAll("--","-")
+				+".ics";
+		var dau=new DataOutputStream(new FileOutputStream(fname));
+		dau.writeChars(out.toString());
+		dau.flush();
+		dau.close();
+	}
 
 	@Autowired
 	VersionHistoryRepository versionHistoryRepository;
